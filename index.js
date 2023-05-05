c4206f56fb65fd978e253645c035cda72f49b15d
Avyyukt <avyyukt.ajith@gmail.com>
2023-05-05 11:24:51 +0530

    feat: add game loop

--- index.js
+++ index.js
@@ -23,7 +23,12 @@ function defaultSnakePosition() {
     const midx = Math.floor(gridsize / 2);
     const midy = midx;
 
+    // It's important that midx+1 is at the front of the array, 
+    // because that's the head of the snake
+    // and we are setting the direction to D_DOWN by default
+    // so the head has to be at the bottom of the grid
     const snake = [[midx+1, midy], [midx, midy], [midx-1, midy]];
+
     return snake;
 }
 
@@ -60,15 +65,23 @@ class Snake {
         }
     }
 
+    isOppositeDirection(direction) {
+        return (
+            (direction === D_DOWN && this.direction === D_UP)
+            || (direction === D_UP && this.direction === D_DOWN)
+            || (direction === D_LEFT && this.direction === D_RIGHT)
+            || (direction === D_RIGHT && this.direction === D_LEFT)
+        );
+    }
+
     updatePosition(direction) {
-        // We can't go in the opposite direction, so we early return
-        if (direction === D_DOWN && this.direction === D_UP) return R_ERR_OPPOSITE_DIRECTION;
-        if (direction === D_UP && this.direction === D_DOWN) return R_ERR_OPPOSITE_DIRECTION;
-        if (direction === D_LEFT && this.direction === D_RIGHT) return R_ERR_OPPOSITE_DIRECTION;
-        if (direction === D_RIGHT && this.direction === D_LEFT) return R_ERR_OPPOSITE_DIRECTION;
+        // We can't go in the opposite direction, so we check this case
+        const isOppositeDirection = this.isOppositeDirection(direction);
+
 
         // Check if direction is null or undefined, and then set it to the current direction
-        if (direction === null || direction === undefined) {
+        // if opposite direction, we ignore this and set it to the current direction
+        if (direction === null || direction === undefined || isOppositeDirection) {
             direction = this.direction;
         }
 
@@ -92,28 +105,77 @@ class Snake {
         this.coords.pop();
 
         this.direction = direction;
+
+        if (isOppositeDirection) {
+            return R_ERR_OPPOSITE_DIRECTION;
+        }
+
+        return R_SNAKE_UPDATE_SUCCESSFUL;
     }
 
 }
 
+class SpawnManager {
+    constructor() {
+        this.blocks = [];
+    }
+}
 
-let snake = new Snake();
-snake.draw();
-
-document.addEventListener('keydown', (event) => {
-
-    snake.clear();
-    let ret;
-    switch (event.key) {
-        case 'ArrowUp': ret = snake.updatePosition(D_UP); break;
-        case 'ArrowRight': ret = snake.updatePosition(D_RIGHT); break;
-        case 'ArrowDown': ret = snake.updatePosition(D_DOWN); break;
-        case 'ArrowLeft': ret = snake.updatePosition(D_LEFT); break;
-        default: break;
+class GameLoop {
+    constructor() {
+        this.snake = new Snake();
+        this.spawnManager = new SpawnManager();
+        this.queue = [];
     }
 
-    if (ret === R_ERR_OPPOSITE_DIRECTION) { console.log("Opposite direction detected!"); }
-    snake.draw();
+    handleClick(event) {
+        // Check if event is any one of the arrow keys, and only then push it to the queue
+        // Disregard all other keys
+        let key = event.key;
 
+        // If key is not one of the arrow keys, return
+        if (key !== 'ArrowUp' && key !== 'ArrowRight' && key !== 'ArrowDown' && key !== 'ArrowLeft') return;
 
+        // If the direction is opposite, we disregard this and don't push it into our key queue
+        const dir = (key === 'ArrowUp') ? D_UP : (key === 'ArrowRight') ? D_RIGHT : (key === 'ArrowDown') ? D_DOWN : D_LEFT;
+        if (this.snake.isOppositeDirection(dir)) {
+            console.log("[INFO] Disregarding opposite direction keypress from key queue");
+            return;
+        }
+
+        this.queue.unshift(event);
+    }
+
+    tick() {
+        this.snake.clear();
+        let ret;
+
+        // Check if queue is empty
+        if (this.queue.length === 0) {
+            ret = this.snake.updatePosition(null);
+        } else {
+            let event = this.queue.pop();
+            switch (event.key) {
+                case 'ArrowUp': ret = this.snake.updatePosition(D_UP); break;
+                case 'ArrowRight': ret = this.snake.updatePosition(D_RIGHT); break;
+                case 'ArrowDown': ret = this.snake.updatePosition(D_DOWN); break;
+                case 'ArrowLeft': ret = this.snake.updatePosition(D_LEFT); break;
+                default: break;
+            }
+        }
+
+        if (ret === R_ERR_OPPOSITE_DIRECTION) { console.log("Opposite direction detected!"); }
+        this.snake.draw();
+    }
+}
+
+// let snake = new Snake();
+let loop = new GameLoop();
+
+document.addEventListener('keydown', (event) => {
+    loop.handleClick(event);
 });
+
+let interval = setInterval(() => {
+    loop.tick();
+}, 375);
